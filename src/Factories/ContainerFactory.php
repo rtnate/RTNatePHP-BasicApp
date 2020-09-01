0781c3a55b8821dee69c345f8dda324fc7c4960d
Nate Taylor <nate@rtelectronix.com>
2020-09-01 14:42:37 -0500

    Polished and documented ContainerFactory and SlimFactory

--- src/Factories/ContainerFactory.php
+++ src/Factories/ContainerFactory.php
@@ -1,21 +1,61 @@
 <?php
 
+/**
+ * ConatinerFactory Class Definition
+  
+ * PHP version 5
+ *
+ * LICENSE: MIT (see ../../LICENSE) 
+ *
+ * @package    rtnatephp/basic-app
+ * @author     Nate Taylor <nate@rtelectronix.com
+ * @copyright  2020 - Nate Taylor
+ * @license    MIT (see ../../LICENSE) 
+ * @link       http://www.github.com/rtnate/RTNatePHP-BasicApp/
+ *
+ */
+
 namespace RTNatePHP\BasicApp\Factories;
 
+/**
+ * Factory class for building the PHP-DI container
+ * 
+ * @see \DI\ContainerBuilder
+ */
 class ContainerFactory
 {
+    /**
+     * PHP-DI ContainerBuilder instance
+     *
+     * @var \DI\ContainerBuilder
+     */
     protected $builder;
     
+    /**
+     * Constuct a new ContainerFactory
+     */
     public function __construct()
     {
         $this->builder = new \DI\ContainerBuilder();
     }
 
+    /**
+     * Build the container
+     *
+     * @return \DI\Container
+     */
     public function build()
     {
         return $this->builder->build();
     }
 
+    /**
+     * Add PHP-DI definitions to the container
+     *
+     * @param array $definitions Definitions to add into the container
+     * @return void
+     * @see http://php-di.org/doc/definition.html
+     */
     public function add(array $definitions)
     {
         $this->builder->addDefinitions($definitions);
--- src/Factories/SlimFactory.php
+++ src/Factories/SlimFactory.php
@@ -1,4 +1,18 @@
 <?php 
+/**
+ * SlimFactory Class Definition
+  
+ * PHP version 5
+ *
+ * LICENSE: MIT (see ../../LICENSE) 
+ *
+ * @package    rtnatephp/basic-app
+ * @author     Nate Taylor <nate@rtelectronix.com
+ * @copyright  2020 - Nate Taylor
+ * @license    MIT (see ../../LICENSE) 
+ * @link       http://www.github.com/rtnate/RTNatePHP-BasicApp/
+ *
+ */
 
 namespace RTNatePHP\BasicApp\Factories;
 
@@ -6,26 +20,57 @@ use \DI\Bridge\Slim\Bridge as SlimDiBridge;
 use \DI\Container;
 use Illuminate\Support\Arr;
 
+/**
+ * Factory class from building a \Slim\App 
+ * 
+ * @see http://www.slimframework.com/docs/v4/
+ */
 class SlimFactory
 {
     protected $slim;
 
+    /**
+     * Slim error middleware instance
+     *
+     * @var \Slim\Middleware\ErrorMiddleware|null
+     */
     protected $error_middleware = null;
 
     protected $error_renderers = [];
 
+    /**
+     * Slim app's errror handler
+     *
+     * @var \Slim\Handlers\ErrorHandler|null
+     */
     protected $error_handler = null;
 
     protected $global_middleware = [];
 
+    /**
+     * Slim app global debug flag
+     *
+     * @var bool
+     */
     protected $debug;
 
+    /**
+     * Create a new SlimFactory 
+     *
+     * @param Container $container The PHP-DI Container 
+     * @param array $options Set ['debug' => true] to enable debugging
+     */
     public function __construct(Container $container, $options = [])
     {   
         $this->debug = Arr::get($options, 'debug', false);
         $this->slim = SlimDiBridge::create($container);
     }
 
+    /**
+     * Add the Slim Error Middleware to the app
+     *
+     * @return void
+     */
     protected function addErrorMiddleware()
     {
         if (!$this->error_middleware) $this->error_middleware = $this->slim->addErrorMiddleware($this->debug, true, true);
@@ -33,10 +78,16 @@ class SlimFactory
         else $this->error_handler = $this->error_middleware->getDefaultErrorHandler();
         foreach($this->error_renderers as $type => $renderer)
         {
-            $this->error_handler->registerErrorRenderer($type, $renderer);
+            if ($this->error_handler instanceof \Slim\Handlers\ErrorHandler)
+                $this->error_handler->registerErrorRenderer($type, $renderer);
         }
     }
 
+    /**
+     * Load any supplied middleware into the slim instance
+     *
+     * @return void
+     */
     protected function loadMiddleware()
     {
         foreach($this->global_middleware as $key => $middleware)
@@ -45,16 +96,34 @@ class SlimFactory
         }
     }
 
+    /**
+     * Add global middleware to the slim app
+     *
+     * @param \Psr\Http\Server\MiddlewareInterface|string|callable $middleware
+     * @return void
+     */
     public function addGlobalMiddleware($middleware)
     {
         array_push($this->global_middleware, $middleware);
     }
 
+    /**
+     * Add an error rendered for the supplied content type
+     *
+     * @param string $contentType
+     * @param \Psr\Http\Server\MiddlewareInterface|string|callable $renderer
+     * @return void
+     */
     public function addErrorRenderer($contentType, $renderer)
     {
         $this->error_renderers[$contentType] =  $renderer;
     }
 
+    /**
+     * Build the slim app
+     *
+     * @return \Slim\App The Slim app
+     */
     public function build()
     {
         $this->slim->addRoutingMiddleware();
@@ -63,7 +132,12 @@ class SlimFactory
         return $this->slim;
     }
 
-
+    /**
+     * Shortcut function for creating a Slim app from the supplied PHP-DI container
+     *
+     * @param Container $container The PHP-DI instance
+     * @return \Slim\App The Slim app instance
+     */
     static function create(Container $container)
     {
         $factory = new static($container);
